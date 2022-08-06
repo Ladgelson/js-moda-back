ecb2813efac3402af6683e9b872d8b4ec2e7a1d2
ladgelson <micaiasladgelsondasilva@gmail.com>
2022-08-06 15:24:07 -0300

    feat: creating basic structure

--- src/main/java/com/jsmodabackend/controller/swagger/CategoryController.java
+++ src/main/java/com/jsmodabackend/controller/swagger/CategoryController.java
@@ -1,7 +1,6 @@
 package com.jsmodabackend.controller.swagger;
 
 import com.jsmodabackend.dto.CategoryDTO;
-import com.jsmodabackend.dto.ProductDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,12 +8,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.awt.print.Pageable;
+
 
 @Tag(name = "Category", description = "Resources to manage data about Categories")
 @RequestMapping("/api/v1/categories")
